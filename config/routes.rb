d6db2ff20bf3520d995dbb82dde7ed1fca352a6a
narumifumiya <124240213+narumifumiya@users.noreply.github.com>
2023-03-30 11:56:27 +0000

    指定した日の記録(投稿数)を非同期で表示させる

--- config/routes.rb
+++ config/routes.rb
@@ -13,11 +13,12 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: [:index,:show,:edit,:update] do
+    get "search" => "users#search"
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
   end
-  
+
   get "search" => "searches#search"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
